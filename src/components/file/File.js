515894ee178f56bea5c64e62117f9e6d0543f5c1
Abdul1810 <abdulking8010@gmail.com>
2023-07-20 23:36:42 +0530

    POPOPOPOP

--- src/components/file/File.js
+++ src/components/file/File.js
@@ -1,26 +1,23 @@
 // components/FileManager.js
 'use client';
-import React, { useState } from 'react';
 import FileItem from './FileItem';
 import useSWR from 'swr';
 
-const FileManager = () => {
-  const fetcher = url => fetch(url).then(res => res.json());
-  const { data: file, error, isLoading } = useSWR('https://api.github.com/repos/vercel/swr', fetcher) || [];
-  if (error) return <div>failed to load</div>
-  if (isLoading) return <div>loading...</div>
+function FileManager() {
+	const fetcher = url => fetch(url).then(res => res.json());
+	const { data: files, error, isLoading } = useSWR('/api/list-data', fetcher) || [];
+	if (error) return <div>{`${error}`}</div>
+	if (isLoading) return <div>loading...</div>
 
-  return (
-    <div className="p-4">
-      <div>
-        {/* {files.map((file, index) => ( */}
-          <FileItem
-            name={file.name}
-          />
-        {/* ))} */}
-      </div>
-    </div>
-  );
+	return (
+		<div className="p-4">
+			<div>
+				{files.map((file, index) => (
+					<FileItem key={index} name={file.name} updated_at={file.updated_at} size={file.size} />
+				))}
+			</div>
+		</div>
+	);
 };
 
 export default FileManager;
--- src/components/file/FileItem.js
+++ src/components/file/FileItem.js
@@ -1,13 +1,15 @@
 // components/FileItem.js
 import React from 'react';
 
-const FileItem = ({ name, onClick }) => {
+const FileItem = ({ name, updated_at, size }) => {
   const icon = '📄';
 
   return (
     <div className="cursor-pointer" onClick={onClick}>
       <span className="mr-2">{icon}</span>
-      {name}
+      <span className="mr-2">{name}</span>
+      <span className="ml-2 text-xs text-gray-500">{updated_at}</span>
+      <span className="ml-2 text-xs text-gray-500">{size} bytes</span>
     </div>
   );
 };
