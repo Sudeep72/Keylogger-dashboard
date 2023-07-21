049b91daa998c6868b8db918dffedc47c8a82d18
Sudeep <sudeep7217@gmail.com>
2023-07-21 17:44:13 +0530

    typewriting effect

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,9 @@
 'use client';
-
+import React from 'react';
 import Auth from 'src/components/Auth';
 import { useAuth, VIEWS } from 'src/components/AuthProvider';
 import File from '../components/file/File';
+import Typewriter from 'typewriter-effect';
 
 export default function Home() {
   const { initial, user, view, signOut } = useAuth();
@@ -18,15 +19,26 @@ export default function Home() {
   if (user) {
     return (
       <div>
-        <h2 className="text-4xl font-extrabold  text-center pt-5 py-7 font-raleway">It's time to wake up and smell the <span className='text-primary '>Mutating Hash </span>!!</h2>
+       <h2 className="text-4xl font-extrabold text-center pt-5 py-7 font-raleway">
+          <span>It's time to wake up and smell the </span>
+          <span className='text-primary'>
+            <Typewriter
+              options={{
+                strings: ['Mutating Hash!!', 'Evolving Hash!!'],
+                autoStart: true,
+                loop: true,
+              }}
+            />
+          </span>
+        </h2>
         <File />
         <div className='items-center justify-center flex flex-col pt-3'>
-        <button
-          className="w-32 px-4 py-2 mt-4 btn btn-outline btn-primary"
-          onClick={signOut}
-        >
-          Sign Out
-        </button>
+          <button
+            className="w-32 px-4 py-2 mt-4 btn btn-outline btn-primary"
+            onClick={signOut}
+          >
+            Sign Out
+          </button>
         </div>
       </div>
     );
