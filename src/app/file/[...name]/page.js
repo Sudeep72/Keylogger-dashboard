c9fbae7bff558a1793361f71ed9bb736ad8383b1
Sudeep <sudeep7217@gmail.com>
2023-07-22 22:04:11 +0530

    display navbar

--- src/app/file/[...name]/page.js
+++ src/app/file/[...name]/page.js
@@ -1,77 +1,87 @@
 import { notFound } from 'next/navigation';
-
 import supabaseServer from '@/lib/supabase-server';
+import Navbar from '@/components/layout/Display_Navbar';
 
-const fetcher = url => fetch(url).then(res => res.json());
+const fetcher = (url) => fetch(url).then((res) => res.json());
 
 async function getFileFromParams(params) {
-	const name = params?.name?.join("/")
-	const file = await fetcher(`http://localhost:3000/api/search-data?pc_name=${name}`)
+  const name = params?.name?.join('/');
+  const file = await fetcher(`http://localhost:3000/api/search-data?pc_name=${name}`);
 
-	if (!file) {
-		null
-	}
+  if (!file) {
+    null;
+  }
 
-	return file
+  return file;
 }
 
-export async function generateMetadata({
-	params,
-}) {
-	const file = await getFileFromParams(params)
+export async function generateMetadata({ params }) {
+  const file = await getFileFromParams(params);
 
-	if (!file) {
-		return {}
-	}
+  if (!file) {
+    return {};
+  }
 
-	return {
-		title: file.title,
-		description: file.description,
-	}
+  return {
+    title: file.title,
+    description: file.description,
+  };
 }
 
 export async function generateStaticParams() {
-	const allFiles = await fetcher(`http://localhost:3000/api/list-data`)
-	return allFiles.map((file) => ({
-		params: {
-			name: file.name.split("/"),
-		},
-	}))
+  const allFiles = await fetcher(`http://localhost:3000/api/list-data`);
+  return allFiles.map((file) => ({
+    params: {
+      name: file.name.split('/'),
+    },
+  }));
 }
 
 export default async function FilePage({ params }) {
-	// check if user is logged in
-	const user = await supabaseServer().auth.getUser();
+  // check if user is logged in
+  const user = await supabaseServer().auth.getUser();
 
-	if (!user) {
-		return {
-			redirect: {
-				destination: '/',
-				permanent: false,
-			},
-		}
-	}
+  if (!user) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    };
+  }
 
+  const file = await getFileFromParams(params);
 
-	const file = await getFileFromParams(params)
+  if (!file) {
+    notFound();
+  }
 
-	if (!file) {
-		notFound()
-	}
+  const content = await fetcher(
+    `http://localhost:3000/api/download-data?pc_name=${params.name}`
+  );
+  const formattedDate = new Date(file.description).toLocaleDateString(undefined, {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+  });
 
-	const content = await fetcher(`http://localhost:3000/api/download-data?pc_name=${params.name}`)
-	const formattedDate = new Date(file.description).toLocaleDateString(undefined, {
-		weekday: 'long',
-		year: 'numeric',
-		month: 'short',
-		day: 'numeric',
-	  });
-
-	return (
-		<div>
-			<h1>{file.title}</h1>
-			<p>{formattedDate}</p>
-			<pre>{content}</pre>
-		</div>
-	)
+  return (
+    <div className="min-h-screen bg-base-200">
+      <Navbar />
+      <div>
+        <div className="hero text-center">
+          <div className="max-w-md">
+            <h4 className="py-4 text-2xl font-bold">{file.title}</h4>
+            <span className="my-1 mb-2 text-lg font-medium text-gray-500">
+              Last Updated at: {formattedDate}
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className="mx-8 my-4 items-start text-left">
+        <span className="mb-2 text-xl font-medium ">{content}</span>
+      </div>
+    </div>
+  );
 }
---
+++ src/components/layout/Display_Navbar.js
@@ -0,0 +1,70 @@
+export default function Display() {
+  function toggleMenu() {
+    const dropdownMenu = document.getElementById("dropdown-menu");
+    dropdownMenu.classList.toggle("hidden");
+  }
+
+  return (
+    <>
+      <div className="hero text-center">
+        <div className="navbar">
+          <div className="navbar-start">
+            <div className="dropdown lg:hidden">
+              <label
+                tabIndex={0}
+                className="btn-ghost btn-circle btn"
+                onClick={toggleMenu}
+                onKeyDown={(e) => {
+                  if (e.key === "Enter" || e.key === " ") {
+                    toggleMenu();
+                  }
+                }}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h16M4 18h7"
+                  />
+                </svg>
+              </label>
+              <ul
+                id="dropdown-menu"
+                className="dropdown-content menu rounded-box menu-sm z-[1] mt-3 w-52 bg-base-100 p-2 shadow hidden"
+              >
+                <li>
+                  <a>Go Back</a>
+                </li>
+                <li>
+                  <a>Download</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="navbar-center">
+          <h2 className="py-7 pt-5 text-center font-poppins text-4xl font-extrabold">
+            Information is <span className="text-primary">Wealth</span>
+          </h2>
+        </div>
+      </div>
+      <div className="right-0 top-0 m-4 hidden pt-2 lg:absolute lg:block xl:block 2xl:block">
+        <button className="btn-primary btn-outline btn px-4 py-2" onClick={''}>
+          Go Back
+        </button>
+      </div>
+      <div className="right-32 top-0 m-4 hidden pt-2 lg:absolute lg:block xl:block 2xl:block">
+        <button className="btn-primary btn-outline btn px-4 py-2" onClick={''}>
+         Download
+        </button>
+      </div>
+    </>
+  );
+}
