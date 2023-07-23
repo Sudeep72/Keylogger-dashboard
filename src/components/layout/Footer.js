ef0e9c98337d312e5d6dd00942497f9e4175d8b2
Sudeep <sudeep7217@gmail.com>
2023-07-23 12:42:53 +0530

    toast

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,57 +1,92 @@
+import React, { useState } from 'react';
+
 export default function Footer() {
-	const email_reg = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+  const email_reg = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+  const [showToast, setShowToast] = useState(false);
+  const [toastMessage, setToastMessage] = useState('');
+  const [toastType, setToastType] = useState('');
 
-	const handleSubmit = (e) => {
-		e.preventDefault();
-		const email = e.target.mail.value;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const email = e.target.mail.value;
 
-		if (!email_reg.test(email)) {
-			alert('Please enter a valid email address');
-			return;
-		} else {
-			const data = {
-				mail: email,
-			};
+    if (!email_reg.test(email)) {
+      setShowToast(true);
+      setToastMessage('Enter a valid email address!');
+      setToastType('warning');
 
-			const JSONdata = JSON.stringify(data)
+      setTimeout(() => {
+        setShowToast(false);
+      }, 3000);
+    } else {
+      const data = {
+        mail: email,
+      };
 
-			const endpoint = '/api/send-query'
+      const JSONdata = JSON.stringify(data);
 
-			const options = {
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json',
-				},
-				body: JSONdata,
-			}
+      const endpoint = "/api/send-query";
 
-			const response = fetch(endpoint, options).then((res) => res.json())
+      const options = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSONdata,
+      };
 
-			if (response.status === 'ok') {
-				alert('Success!')
-			} else {
-				alert('Something went wrong.')
-			}
+      fetch(endpoint, options)
+        .then((res) => res.json())
+        .then((response) => {
+          if (response.status === "ok") {
+            setShowToast(true);
+            setToastMessage('Success');
+            setToastType('success');
 
-		}
-	};
+            setTimeout(() => {
+              setShowToast(false);
+            }, 3000);
+          } else {
+            setShowToast(true);
+            setToastMessage('Err! Something went wrong');
+            setToastType('error');
 
-	return (
-		<div className="fixed flex flex-col items-center justify-center inset-x-0 bottom-0 p-4 text-white mx-auto mt-8">
-			<h4 className="mb-2 font-poppins">Need to Register or any Queries?</h4>
-			<div className="relative">
-				<form className="form-control" method="POST" onSubmit={handleSubmit}>
-					<input
-						type="text"
-						placeholder="username@site.com"
-						name="mail"
-						id="mail"
-						autoComplete="on"
-						className="input input-primary w-auto pr-16"
-					/>
-					<button type="submit" className="btn btn-primary absolute right-0 rounded-l-none">Airwave</button>
-				</form>
-			</div>
-		</div>
-	);
+            setTimeout(() => {
+              setShowToast(false);
+            }, 3000);
+          }
+        });
+    }
+  };
+
+  return (
+    <div className="fixed inset-x-0 bottom-0 mx-auto mt-8 flex flex-col items-center justify-center p-4 text-white">
+      <h4 className="mb-2 font-poppins">Need to Register or any Queries?</h4>
+      <div className="relative">
+        <form className="form-control" method="POST" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="username@site.com"
+            name="mail"
+            id="mail"
+            autoComplete="on"
+            className="input-primary input w-auto pr-16"
+          />
+          <button
+            type="submit"
+            className="btn-primary btn absolute right-0 rounded-l-none"
+          >
+            Airwave
+          </button>
+        </form>
+      </div>
+      {showToast && (
+        <div className={`toast-end toast`}>
+          <div className={`alert alert-${toastType}`}>
+            <span>{toastMessage}</span>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
